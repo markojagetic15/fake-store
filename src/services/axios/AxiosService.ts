6efc9f6486d0bc8541d4ece4d0f61ef59d069dcc
Marko Jagetic <marko.jagetic3@gmail.com>
2024-06-15 21:32:45 +0200

    Add github PR template

--- src/services/axios/AxiosService.ts
+++ src/services/axios/AxiosService.ts
@@ -1,30 +1,30 @@
+import { Deserializer } from 'jsonapi-serializer'
 import { HttpAdapter } from './httpsAdapterServices.ts'
-// import { Deserializer } from 'jsonapi-serializer'
-// import { Product } from 'entities/product'
+import { IProduct } from '@entities/product'
 
 export class AxiosService extends HttpAdapter {
   constructor() {
     super()
   }
 
-  // getAnyDeserializedData(e: Product[]) {
-  //   try {
-  //     return new Deserializer({
-  //       keyForAttribute: 'camelCase',
-  //       pluralizeType: false,
-  //     })
-  //       .deserialize(e)
-  //       .then((data) => {
-  //         return data
-  //       })
-  //   } catch (error: unknown) {
-  //     if (error instanceof Error) {
-  //       // Handle the error specifically if needed
-  //       console.error(error.message)
-  //     }
-  //     return null
-  //   }
-  // }
+  getAnyDeserializedData(e: IProduct[]) {
+    try {
+      return new Deserializer({
+        keyForAttribute: 'camelCase',
+        pluralizeType: false,
+      })
+        .deserialize(e)
+        .then((data) => {
+          return data
+        })
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        // Handle the error specifically if needed
+        console.error(error.message)
+      }
+      return null
+    }
+  }
 
   async getProducts() {
     const response = await this.GET('https://fakestoreapi.com/products')
